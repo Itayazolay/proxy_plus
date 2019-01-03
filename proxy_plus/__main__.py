6826facf59bc515a2b40b5377cfec6209a721820
itay <itayazolay@gmail.com>
2019-01-03 21:10:10 +0200

    proxy_plus-2 Add CLI options

--- proxy_plus/__main__.py
+++ proxy_plus/__main__.py
@@ -5,7 +5,13 @@ import sys
 import click
 import asyncio
 import proxy_plus
-import signal
+
+try:
+    import uvloop
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    pass
 
 loop = asyncio.get_event_loop()
 
@@ -15,35 +21,32 @@ def main(args=None):
     """Console script for proxy_plus."""
     pass
 
+
+def async_command(func):
+    def wrapper(*args, **kwargs):
+        try:
+            loop.run_until_complete(func(*args, **kwargs))
+        except KeyboardInterrupt:
+            pass
+        finally:
+            loop.stop()
+
+    return wrapper
+
+
 @click.command()
-@click.option("--lhost", default="")
+@click.option("--lhost", default="127.0.0.1")
 @click.option("--lport", default=0)
 @click.option("--rhost")
 @click.option("--rport")
-def lpf(lhost, lport, rhost, rport):
-
-    loop.run_until_complete(
-        proxy_plus.start_local_port_forwarding(
+@async_command
+async def listen_connect(lhost, lport, rhost, rport):
+        server = await proxy_plus.start_local_port_forwarding(
             (lhost, lport), loop.create_connection,
-            (rhost, rport)
-        )
-    )
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        loop.close()
-    return 0
+            (rhost, rport))
+        await server.wait_closed()
 
-@click.command()
-@click.option('-s', '--string-to-echo', 'string')
-def echo(string):
-    click.echo(string)
 
 if __name__ == "__main__":
-    for signame in ('SIGINT', 'SIGTERM'):
-
-        signal.signal(getattr(signal, signame),
-                    lambda a: loop.stop())
-    main.add_command(echo)
-    main.add_command(lpf)
+    main.add_command(listen_connect)
     sys.exit(main())  # pragma: no cover
